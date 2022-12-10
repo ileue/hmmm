0af0f8fddedf69521d30a4f260481ff7c08970b1
Przemek <krasnal187@gmail.com>
2022-12-10 18:26:35 +0100

    post buttons revamp

--- web/components/Post/PostImageSlider.vue
+++ web/components/Post/PostImageSlider.vue
@@ -1,13 +1,13 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="relative">
-    <div
-      class="text-transparent rounded overflow-hidden relative bg-neutral-200/50 dark:bg-neutral-800/50 aspect-video"
+  <PostImageWrapper>
+    <Transition
+      enter-from-class="translate-x-full"
+      enter-active-class="transition-transform"
+      leave-to-class="-translate-x-full"
+      leave-active-class="transition-transform"
     >
-      <Transition
-        enter-from-class="translate-x-full"
-        enter-active-class="transition-transform"
-        leave-to-class="-translate-x-full"
-      ></Transition>
-    </div>
-  </div>
+    </Transition>
+  </PostImageWrapper>
 </template>
